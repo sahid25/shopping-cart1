00494f02a5c3f265316de54c1ec47ab447dd7c02
sahid25 <ashrafsahid25@gmail.com>
2022-01-23 22:11:21 +0600

    50% done now

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
-// plus function
-function plusValue(id) {
-    let thisId = parseInt(document.getElementById(id).value, 10);
-    thisId++;
-    document.getElementById(id).value = thisId++;
-}
-
-// Minus function
-
-function minusValue(id) {
-    let minusId = parseInt(document.getElementById('input1').value);
-    minusId--;
-    document.getElementById('input1').value = minusId--;
-}
-// Display none
-function displayNone(id) {
-    const idName = document.getElementById(id).style.display = 'none';
-
-}
 
 
-//  remove Elements
-const removeItem = document.getElementById('removeItem1').addEventListener('click', function () {
-    displayNone('cartItem1');
-});
-
-const removeItem2 = document.getElementById('removeItem2').addEventListener('click', function () {
-    displayNone('cartItem2');
-});
-// Pluse Value
-const btnPlus = document.getElementById('btnPlus').addEventListener('click', function () {
-    plusValue('input1');
+const removeItem1 = document.getElementById('removeItem1').addEventListener('click', function () {
+    const cartItem1 = document.getElementById('cartItem1').style.display = 'none';
+    let subTotal = document.getElementById('subTotal').innerText = '00';
 
 })
-// Minus Value
-const btnMinus = document.getElementById('btnMinus').addEventListener('click', function () {
-    minusValue('input1')
+
+const removeItem2 = document.getElementById('removeItem2').addEventListener('click', function () {
+    const cartItem2 = document.getElementById('cartItem2').style.display = 'none';
+    let subTotal = document.getElementById('subTotal').innerText = '00';
 });
 
+// const phnPlus = document.getElementById('phnPlus').addEventListener('click', function () {
+//     handleProductChange('phone', 'phnCost', true );
+// });
+// const phnMinus = document.getElementById('phnMinus').addEventListener('click', function () {
+//     handleProductChange('phone', 'phnCost', false );
+// });
 
+// const casePlus = document.getElementById('casePlus').addEventListener('click', function () {
+//     handleProductChange('case', 'caseCost', true );
+// });
+// const caseMinus = document.getElementById('caseMinus').addEventListener('click', function () {
+//     handleProductChange('case', 'caseCost', false );
+// });
+
+function handleProductChange(id, outputId, isIncrese, subtotalId) {
+    let product = document.getElementById(id + 'Input');
+    let productCount = parseInt(product.value);
+    let productNewCount = productCount;
+    if (isIncrese == true) {
+        productNewCount = productCount + 1;
+        
+    }
+    if (isIncrese == false && productCount > 0) {
+        productNewCount = productCount - 1;
+
+    }
+    product.value = productNewCount;
+    // const countNumber = productNewCount * numberIs;
+    let productTotal = 0;
+    if (id == 'phone') {
+        productTotal = productNewCount * 1219;
+    }
+    if (id == 'case') {
+        productTotal = productNewCount * 59;
+    }
+        const totalValue = document.getElementById(outputId).innerText = productTotal;
+        // const totaL = document.getElementById(subtotalId).innerText = productTotal ;
+}
